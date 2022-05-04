0a9e09e9d4fb5d39673355ac9bca7071ef75c507
Dwarf-workroom <101054441+Dwarf-workroom@users.noreply.github.com>
2022-05-04 10:51:07 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+  include_pakage_data = True
+  name= 'testtest1234',
+  version='1.0.0',
+  description='oss-dev my calculator 001',
+  author= 'dwarfhand',
+  author_email='1923857@donga.ac.kr',
+  url = 'https://github.com/Dwarf-workroom/testoutput/new/main',
+  install_requires = ['pytest'],
+  long_description = 'oss development...',
+  long_description = 'text/markdown',
+  classifiers = [
+    "programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+    ],
+  }
